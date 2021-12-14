93a1056ba9b368236e4cad982eca220e9b5afe92
musmuliadi08 <musliadics12@gmail.com>
2021-12-14 22:28:36 +0800

    merubah script js op aritmatika & menjalankan demo op penugasan

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Operator Aritmatika
 function OpAritmatika(){
 	var hasil;
 
@@ -10,138 +11,236 @@ function OpAritmatika(){
 	var d = Number(VarB);
 
 	if(JOperator == "+"){
-		if(document.getElementById('CheckboxVarA').checked && document.getElementById('CheckboxVarB').checked){
+		if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked && document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = VarA + VarB;
-			console.log(hasil);
-			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Penjumlahan dari "'+ VarA +'" + "'+ VarB +'" adalah = '+ hasil;
+			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Penjumlahan dari "'+ VarA +'" + "'+ VarB +'" adalah = "'+ hasil +'"';
 			form.reset();
-		}else if(document.getElementById('CheckboxVarB').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = c + VarB;
-			console.log(hasil);
-			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Penjumlahan dari '+ c +' + "'+ VarB +'" adalah = '+ hasil;
+			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Penjumlahan dari '+ c +' + "'+ VarB +'" adalah = "'+ hasil +'"';
 			form.reset();
-		}else if(document.getElementById('CheckboxVarA').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked){
 			hasil = VarA + d;
-			console.log(hasil);
-			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Penjumlahan dari "'+ VarA +'" + '+ d +' adalah = '+ hasil;
+			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Penjumlahan dari "'+ VarA +'" + '+ d +' adalah = "'+ hasil + '"';
 			form.reset();
 		}else{
 			hasil = c + d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = "Hasil Penjumlahan dari "+ c +" + "+ d +" adalah = "+ hasil;
 			form.reset();
 		}
 	}else if(JOperator == "-"){
-		if(document.getElementById('CheckboxVarA').checked && document.getElementById('CheckboxVarB').checked){
+		if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked && document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = VarA - VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pengurangan dari "'+ VarA +'" - "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarB').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = c - VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pengurangan dari '+ c +' - "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarA').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked){
 			hasil = VarA - d;
-			console.log(hasil);
-			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pengurangan dari "	'+ VarA +'" - '+ d +' adalah = '+ hasil;
+			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pengurangan dari "'+ VarA +'" - '+ d +' adalah = '+ hasil;
 			form.reset();
 		}else{
 			hasil = c - d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = "Hasil Pengurangan dari "+ c +" - "+ d +" adalah = "+ hasil;
 			form.reset();
 		}
 	}else if(JOperator == "*"){
-		if(document.getElementById('CheckboxVarA').checked && document.getElementById('CheckboxVarB').checked){
+		if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked && document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = VarA * VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Perkalian dari "'+ VarA +'" * "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarB').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = c * VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Perkalian dari '+ c +' * "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarA').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked){
 			hasil = VarA * d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Perkalian dari "'+ VarA +'" * '+ d +' adalah = '+ hasil;
 			form.reset();
 		}else{
 			hasil = c * d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = "Hasil Perkalian dari "+ c +" * "+ d +" adalah = "+ hasil;
 			form.reset();
 		}
 	}else if(JOperator == "**"){
-		if(document.getElementById('CheckboxVarA').checked && document.getElementById('CheckboxVarB').checked){
+		if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked && document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = VarA ** VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pemangkatan dari "'+ VarA +'" ** "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarB').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = c ** VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pemangkatan dari '+ c +' ** "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarA').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked){
 			hasil = VarA ** d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pemangkatan dari "'+ VarA +'" ** '+ d +' adalah = '+ hasil;
 			form.reset();
 		}else{
 			hasil = c ** d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = "Hasil Pemangkatan dari "+ c +" ** "+ d +" adalah = "+ hasil;
 			form.reset();
 		}
 	}else if(JOperator == "/"){
-		if(document.getElementById('CheckboxVarA').checked && document.getElementById('CheckboxVarB').checked){
+		if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked && document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = VarA / VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pembagian dari "'+ VarA +'" / "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarB').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = c / VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pembagian dari '+ c +' / "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarA').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked){
 			hasil = VarA / d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Pembagian dari "'+ VarA +'" / '+ d +' adalah = '+ hasil;
 			form.reset();
 		}else{
 			hasil = c / d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = "Hasil Pembagian dari "+ c +" / "+ d +" adalah = "+ hasil;
 			form.reset();
 		}
 	}else if(JOperator == "%"){
-		if(document.getElementById('CheckboxVarA').checked && document.getElementById('CheckboxVarB').checked){
+		if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked && document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = VarA % VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Modulus/Sisa bagi dari "'+ VarA +'" % "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarB').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarB.checked){
 			hasil = c % VarB;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Modulus/Sisa bagi dari '+ c +' % "'+ VarB +'" adalah = '+ hasil;
 			form.reset();
-		}else if(document.getElementById('CheckboxVarA').checked){
+		}else if(document.getElementById('formAritmatika').CBAritmatikaVarA.checked){
 			hasil = VarA % d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = 'Hasil Modulus/Sisa bagi dari "'+ VarA +'" % '+ d +' adalah = '+ hasil;
 			form.reset();
 		}else{
 			hasil = c % d;
-			console.log(hasil);
 			document.getElementById('AritmatikaValue').innerHTML = "Hasil Modulus/Sisa bagi dari "+ c +" % "+ d +" adalah = "+ hasil;
 			form.reset();
 		}
-	}
-	
+	}	
 }
+// end Operator Aritmatika
+
+// Operator Penugasan
+function OpPenugasan(){
+	var form = document.getElementById('formPenugasan');
+	var VarA = document.getElementById('formPenugasan').PenugasanVarA.value;
+	var VarB = document.getElementById('formPenugasan').PenugasanVarB.value;
+	var JOperator = document.getElementById('formPenugasan').SelOpPenugasan.value;
+
+	var c = Number(VarA);
+	var d = Number(VarB);
+
+	if(JOperator == "+="){
+		if(document.getElementById('formPenugasan').CBPenugasanVarA.checked && document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			VarA += VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Ditambah dengan "'+ VarB +'" adalah = "'+ VarA +'"';
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			c += VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Ditambah dengan "'+ VarB +'" adalah = "'+ c +'"';
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarA.checked){
+			VarA += d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Ditambah dengan '+ d +' adalah = "'+ VarA +'"';
+			form.reset();
+		}else{
+			c += d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Ditambah dengan '+ d +' adalah = '+ c;
+			form.reset();
+		}
+	}else if(JOperator == "-="){
+		if(document.getElementById('formPenugasan').CBPenugasanVarA.checked && document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			VarA -= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dikurang dengan "'+ VarB +'" adalah = '+ VarA;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			c -= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dikurang dengan "'+ VarB +'" adalah = '+ c;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarA.checked){
+			VarA -= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dikurang dengan '+ d +' adalah = '+ VarA;
+			form.reset();
+		}else{
+			c -= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dikurang dengan '+ d +' adalah = '+ c;
+			form.reset();
+		}
+	}else if(JOperator == "*="){
+		if(document.getElementById('formPenugasan').CBPenugasanVarA.checked && document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			VarA *= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dikali dengan "'+ VarB +'" adalah = '+ VarA;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			c *= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dikali dengan "'+ VarB +'" adalah = '+ c;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarA.checked){
+			VarA *= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dikali dengan '+ d +' adalah = '+ VarA;
+			form.reset();
+		}else{
+			c *= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dikali dengan '+ d +' adalah = '+ c;
+			form.reset();
+		}
+	}else if(JOperator == "**="){
+		if(document.getElementById('formPenugasan').CBPenugasanVarA.checked && document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			VarA **= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dipangkatkan dengan "'+ VarB +'" adalah = '+ VarA;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			c **= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dipangkatkan dengan "'+ VarB +'" adalah = '+ c;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarA.checked){
+			VarA **= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dipangkatkan dengan '+ d +' adalah = '+ VarA;
+			form.reset();
+		}else{
+			c **= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dipangkatkan dengan '+ d +' adalah = '+ c;
+			form.reset();
+		}
+	}else if(JOperator == "/="){
+		if(document.getElementById('formPenugasan').CBPenugasanVarA.checked && document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			VarA /= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dibagi dengan "'+ VarB +'" adalah = '+ VarA;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			c /= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dibagi dengan "'+ VarB +'" adalah = '+ c;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarA.checked){
+			VarA /= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dibagi dengan '+ d +' adalah = '+ VarA;
+			form.reset();
+		}else{
+			c /= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dibagi dengan '+ d +' adalah = '+ c;
+			form.reset();
+		}
+	}else if(JOperator == "%="){
+		if(document.getElementById('formPenugasan').CBPenugasanVarA.checked && document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			VarA %= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dimodulus dengan "'+ VarB +'" adalah = '+ VarA;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarB.checked){
+			c %= VarB;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dimodulus dengan "'+ VarB +'" adalah = '+ c;
+			form.reset();
+		}else if(document.getElementById('formPenugasan').CBPenugasanVarA.checked){
+			VarA %= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dimodulus dengan '+ d +' adalah = '+ VarA;
+			form.reset();
+		}else{
+			c %= d;
+			document.getElementById('PenugasanValue').innerHTML = 'Hasil Variabel A setelah Diisi dan Dimodulus dengan '+ d +' adalah = '+ c;
+			form.reset();
+		}
+	}	
+}
+// end Operator Penugasan
 
